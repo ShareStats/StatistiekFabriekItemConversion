f2a8ce503bc865ad326d1c0c6c88325f73cfc443
Sharon Klinkenberg <sharonklinkenberg@re-byodm-145-109-104-222.wireless.uva.nl>
2023-08-28 15:40:11 +0200

    turned irrelevant stuff off

--- correct_taxonomy.R
+++ correct_taxonomy.R
@@ -8,33 +8,33 @@ library(stringr)
 
 n = length(all.item.paths)
 
-taxonomy.conversion.table = data.frame(matrix(NA,n, 5))
-
-names(taxonomy.conversion.table) = c("name", "path", "exsection_line", "exsection", "lastWord")
-
-for (i in 1:n) {
-  
-  # print(all.item.paths[i])
-  
-  text <- readLines( all.item.paths[i] )
-  
-  # Search for something in text and return line number
-  # Some usefull key words: exsection Language Type Level
-  line.nr = grep("exsection", text)
-  
-  # print(text[line.nr])
-  
-  taxonomy.conversion.table$path[i]           = all.item.paths[i]
-  taxonomy.conversion.table$exsection_line[i] = line.nr
-  taxonomy.conversion.table$exsection[i]      = text[line.nr]
-
-}
+# taxonomy.conversion.table = data.frame(matrix(NA,n, 5))
+# 
+# names(taxonomy.conversion.table) = c("name", "path", "exsection_line", "exsection", "lastWord")
+# 
+# for (i in 1:n) {
+#   
+#   # print(all.item.paths[i])
+#   
+#   text <- readLines( all.item.paths[i] )
+#   
+#   # Search for something in text and return line number
+#   # Some usefull key words: exsection Language Type Level
+#   line.nr = grep("exsection", text)
+#   
+#   # print(text[line.nr])
+#   
+#   taxonomy.conversion.table$path[i]           = all.item.paths[i]
+#   taxonomy.conversion.table$exsection_line[i] = line.nr
+#   taxonomy.conversion.table$exsection[i]      = text[line.nr]
+# 
+# }
 
 # taxonomy.conversion.table$lastWord = word(str_replace(taxonomy.conversion.table$exsection, ",", " "), -1)
 
-unique.initial.taxonomy = unique(taxonomy.conversion.table$exsection)
-
-taxonomy.lookup.table = data.frame(oldTax = unique(taxonomy.conversion.table$exsection), newTax = "exsection: ")
+# unique.initial.taxonomy = unique(taxonomy.conversion.table$exsection)
+# 
+# taxonomy.lookup.table = data.frame(oldTax = unique(taxonomy.conversion.table$exsection), newTax = "exsection: ")
 
 # Write unique taxonomies paths to file
 
