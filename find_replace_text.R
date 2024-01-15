5ba5c8b9dea7c0e85affe65c8bc0e9f656393ddb
Sharon Klinkenberg <sharonklinkenberg@re-byodm-145-109-102-121.wireless.uva.nl>
2024-01-15 14:25:52 +0100

    corrected all question mark ? unidentified characters

--- find_replace_text.R
+++ find_replace_text.R
@@ -8,13 +8,28 @@ all.item.paths <- list.files(pattern     = ".Rmd",
 n = length(all.item.paths)
 
 # Words to replace
-findReplace = rbind(c("co\\?ffici\\?nten", "coëfficiënten"),
-                    c("u00eb", "ë"), # with \ R searches for ë and not \u00eb
-                    c("\\ë", "ë"),   # excape \
-                    c("\\\\ë", "ë")  # found some with double \\
+findReplace = rbind(c("co\\?ffici\\?nten", "coëfficiënten"), # 1: Correct ë representation in text
+                    c("u00eb", "ë"),                         # 2: with \ R searches for ë and not \u00eb
+                    c("\\ë", "ë"),                           # 3: excape \
+                    c("\\\\ë", "ë"),                         # 4: found some with double \\
+                    c("[:blank:]\\!\\[", "!["),              # 5: Delete whitespace before " !["
+                    c("[:blank:]$", ""),                     # 6: Delete blank at end of line
+                    c("\\?\\?n","één"),                      # 7: ??n -> één
+                    c("pati\\?nten", "patiénten"),           # 8
+                    c("ge\\?nteresseerd","geïnteresseerd"),  # 9
+                    c("correlatieco\\?ffici\\?nt","correlatiecoëfficiënt"), # 10
+                    c("richtingsco\\?ffici\\?nt","richtingscoëfficiënt"),   # 11
+                    c("be\\?nvloedt", "beïnvloedt"),         # 12
+                    c("be\\?nvloed", "beïnvloed"),           # 13
+                    c("\\?meting", "meting"),                # 14
+                    c("\\?voor \\? \\?na", "voor - na"),     # 15
+                    c("\\?voor", "voor"),                    # 16
+                    c("\\?na", "na"),                        # 17
+                    c("\\?D", "D"),                          # 18
+                    c("", "")
                   )
 
-word = 4
+word = 17
 
 find    = findReplace[word, 1]
 replace = findReplace[word, 2]
@@ -29,11 +44,12 @@ for (i in 1:n) {
 
   
   # Only write file if changes need to be made
-  if( !identical(grep(find, text), integer(0) ) ) {
+  if( sum(stringi::stri_detect(str = text, regex = find) ) > 0 ) {
   
     # find -> replace
-    text <- gsub(find, replace, text)
-    line.nr = grep(replace, text)  
+    # text <- gsub(find, replace, text) 
+    text <- stringi::stri_replace(str = text, regex = find, replacement = replace)
+    # line.nr = grep(replace, text)  
   
     print(text[line.nr])
     writeLines(text, all.item.paths[i])
--- find_text.R
+++ find_text.R
@@ -10,11 +10,29 @@ all.item.paths <- list.files(pattern     = ".Rmd",
 n = length(all.item.paths)
 
 # Words to find
-words = c("\!\[")
+words = c("[:blank:]\\!\\[",           # 1
+          "[:blank:]$",                # 2
+          "\\?[a-q]",                  # 3 find questionmark in string
+          "\\?\\?n",                   # 4
+          "pati\\?nten",               # 5
+          "ge\\?nteresseerd",          # 6
+          "correlatieco\\?ffici\\?nt", # 7
+          "richtingsco\\?ffici\\?nt",  # 8
+          "be\\?nvloedt",              # 9
+          "be\\?nvloed",               # 10
+          "\\?meting",                 # 11
+          "\\?voor \\? \\?na",         # 12
+          "\\?voor",                   # 13
+          "\\?na",                     # 14
+          "\\?D"                       # 15
+          )
 
-word = 1
+word = 3
 
 find = words[word]
+find
+
+stringi::stri_detect(str = text, regex = find)
 
 # Work in progress, using stringr to find [ 
 
@@ -24,19 +42,17 @@ for (i in 1:n) {
   text <- readLines( all.item.paths[i] )
   
   # Search for something in text and return line number
-  # Some usefull key words: exsection Language Type Level
+  # Some useful key words: exsection Language Type Level
   
+  # Only show file if changes need to be made
+  if( sum(stringi::stri_detect(str = text, regex = find) ) > 0 ) {
+  
+    line.nr = stringi::stri_detect(str = text, regex = find)
 
-  
-  # Only write file if changes need to be made
-  if( !identical(grep(find, text), integer(0) ) ) {
-  
-    line.nr = grep(replace, text)  
-  
-    print(text[line.nr])
-st
+    print(c(i, text[line.nr]) )
+
   }
   
 }
 
-stringi::stri_detect(str = text, regex = find)
+
