3e4cebf37d71458935ba5517964098ef84de8e7a
Sharon Klinkenberg <sharonklinkenberg@re-byodm-145-109-102-57.wireless.uva.nl>
2024-01-24 13:55:14 +0100

    corrected open ended questions

--- SQRscripts/correctItemsinItembank.sql
+++ SQRscripts/correctItemsinItembank.sql
@@ -1,6 +1,6 @@
-// I'm seeing items that are MultipleChoice that have strings for correct answers 
-// and not the requered numerical indicator for the correct answer option number
-// for example item 82
+/* I'm seeing items that are MultipleChoice that have strings for correct answers 
+   and not the requered numerical indicator for the correct answer option number
+   for example item 82 */
 
 select id,
        correct_answer,
@@ -10,7 +10,7 @@ from   items
 where  CHAR_LENGTH(correct_answer) > 1
 and    JSON_EXTRACT(question, "$.type") = 'MultipleChoice';
 
-// search for item
+/* search for item */
 
 select JSON_EXTRACT(question, "$.question.content[0]") as question,
        answer_options,
@@ -18,9 +18,9 @@ select JSON_EXTRACT(question, "$.question.content[0]") as question,
 from   items
 where  id = 1700;
 
-// correct item, list number starts at 0
-// the items below had the correct answer from the list as literal string.
-// corrected by assigning the correct nr for the correct answer option
+/* correct item, list number starts at 0
+   the items below had the correct answer from the list as literal string.
+   corrected by assigning the correct nr for the correct answer option */
 
 UPDATE items SET correct_answer = 2 WHERE id = 82;
 UPDATE items SET correct_answer = 2 WHERE id = 150;
@@ -31,8 +31,8 @@ UPDATE items SET correct_answer = 3 WHERE id = 996;
 UPDATE items SET correct_answer = 0 WHERE id = 1313;
 UPDATE items SET correct_answer = 4 WHERE id = 1352;
 
-// All left over items are also literal strings but contain the integer number
-// of the float answer option.
+/* All left over items are also literal strings but contain the integer number
+   of the float answer option. */
 
 select id,
        answer_options,
@@ -42,7 +42,7 @@ from   items
 where  CHAR_LENGTH(correct_answer) > 1
 and    JSON_EXTRACT(question, "$.type") = 'MultipleChoice';
 
-// correct in one go
+/* correct in one go */
 
 UPDATE items SET correct_answer = 1 WHERE id = 1700;
 UPDATE items SET correct_answer = 0 WHERE id = 1702;
@@ -97,4 +97,68 @@ UPDATE items SET correct_answer = 0 WHERE id = 1795;
 UPDATE items SET correct_answer = 0 WHERE id = 1797;
 UPDATE items SET correct_answer = 1 WHERE id = 1799;
 
-// Checked, no more MultipleChoice items with incorrect answer
+/* Checked, no more MultipleChoice items with incorrect answer */
+
+/* Take a look at the open questions */
+
+select id,
+       /* JSON_EXTRACT(question, "$.question.content[0]") as question, */
+       correct_answer,
+       JSON_EXTRACT(question, "$.type") as type
+from   items
+where  JSON_EXTRACT(question, "$.type") = 'OpenString';
+
+/* look at question for specific item */
+
+select JSON_EXTRACT(question, "$.question.content[0]") as question,
+       answer_options,
+       correct_answer
+from   items
+where  id = 82;
+
+/* To Do replace , with . and fraction e.g. 3/4 with decimal value and % with decimal value in correct_answer*/
+
+select id,
+       correct_answer,
+       JSON_EXTRACT(question, "$.type") as type
+from   items
+where  JSON_EXTRACT(question, "$.type") = 'OpenString'
+and    correct_answer REGEXP '\\%|,|\\/';
+
+/* Correct items */
+UPDATE items SET correct_answer = 0.3    WHERE id =  651 ;
+UPDATE items SET correct_answer = 0.6    WHERE id =  652 ;
+UPDATE items SET correct_answer = 0.4    WHERE id =  653 ;
+UPDATE items SET correct_answer = 0.113  WHERE id =  654 ;
+UPDATE items SET correct_answer = 0.1265 WHERE id =  655 ;
+UPDATE items SET correct_answer = 0.0025 WHERE id =  656 ;
+UPDATE items SET correct_answer = 0.5    WHERE id =  657 ;
+UPDATE items SET correct_answer = 0.50   WHERE id =  658 ;
+UPDATE items SET correct_answer = 2.74   WHERE id =  665 ;
+UPDATE items SET correct_answer = 0.196  WHERE id =  666 ;
+UPDATE items SET correct_answer = 0.1666 WHERE id =  667 ;
+UPDATE items SET correct_answer = 0.8333 WHERE id =  668 ;
+UPDATE items SET correct_answer = 0.1388 WHERE id =  669 ;
+UPDATE items SET correct_answer = 0.1157 WHERE id =  670 ;
+UPDATE items SET correct_answer = 0.04   WHERE id =  677 ;
+UPDATE items SET correct_answer = 0.497  WHERE id =  686 ;
+UPDATE items SET correct_answer = 0.04   WHERE id =  687 ;
+UPDATE items SET correct_answer = 0.23   WHERE id =  688 ;
+UPDATE items SET correct_answer = 1.2    WHERE id =  763 ;
+UPDATE items SET correct_answer = 0.0046 WHERE id =  765 ;
+UPDATE items SET correct_answer = 0.02   WHERE id =  946 ;
+UPDATE items SET correct_answer = 0.02   WHERE id =  947 ;
+UPDATE items SET correct_answer = 0.06   WHERE id =  949 ;
+UPDATE items SET correct_answer = 0.025  WHERE id =  951 ;
+UPDATE items SET correct_answer = 0.075  WHERE id =  952 ;
+UPDATE items SET correct_answer = 0.471  WHERE id = 1007 ;
+UPDATE items SET correct_answer = 0.230  WHERE id = 1008 ;
+UPDATE items SET correct_answer = 0.701  WHERE id = 1010 ;
+UPDATE items SET correct_answer = 0.471  WHERE id = 1011 ;
+UPDATE items SET correct_answer = 0.230  WHERE id = 1012 ;
+UPDATE items SET correct_answer = 0.471  WHERE id = 1013 ;
+UPDATE items SET correct_answer = 0.701  WHERE id = 1014 ;
+UPDATE items SET correct_answer = 0.0313 WHERE id = 1049 ;
+UPDATE items SET correct_answer = 0.9688 WHERE id = 1051 ;
+
+/* Checked all items, no more literal strings where numeric is needed.*/
--- SQRscripts/handyQueries.sql
+++ SQRscripts/handyQueries.sql
@@ -1,6 +1,6 @@
-// Handy queries to dig through the itembank
+/* Handy queries to dig through the itembank
 
-// Get the total taxonomy tree structure 
+   Get the total taxonomy tree structure */
 
 SELECT CONCAT( REPEAT( ' ', (COUNT(parent.name) - 1) ), node.name) AS name
 FROM tags AS node,
@@ -9,7 +9,7 @@ WHERE node.lft BETWEEN parent.lft AND parent.rght
 GROUP BY node.name
 ORDER BY node.lft;
 
-// Get only the taxonomy structure and ids for a specific category
+/* Get only the taxonomy structure and ids for a specific category */
 
 SELECT node.name, parent.id
 FROM tags AS node,
@@ -18,13 +18,13 @@ WHERE node.lft BETWEEN parent.lft AND parent.rght
 AND parent.name = 'K/V/G classificatie'
 ORDER BY node.lft;
 
-// search for specific text in question stem
+/* search for specific text in question stem */
 
 select JSON_EXTRACT(question, "$.question.content[0]") as question
 from   items
 where  question REGEXP '[[:<:]]download[[:>:]]';
 
-// look at question for specific item
+/* look at question for specific item */
 
 select JSON_EXTRACT(question, "$.question.content[0]") as question,
        answer_options,
@@ -32,11 +32,11 @@ select JSON_EXTRACT(question, "$.question.content[0]") as question,
 from   items
 where  id = 82;
 
-// See all columns in item table
+/* See all columns in item table */
 
 show columns from items;
 
-// look at answers for closed and open questions
+/* look at answers for closed and open questions */
 
 select id,
        correct_answer,
@@ -44,9 +44,9 @@ select id,
        status
 from   items;
 
-// I'm seeing items that are MultipleChoice that have strings for correct answers 
-// and not the requered numerical indicator for the correct answer option number
-// for example item 82
+/* I'm seeing items that are MultipleChoice that have strings for correct answers 
+   and not the requered numerical indicator for the correct answer option number
+   for example item 82 */
 
 select id,
        correct_answer,
@@ -67,7 +67,7 @@ select id,
 from   items
 where  JSON_EXTRACT(question, "$.type") = 'OpenString';
 
-// look at question for specific item
+/* look at question for specific item */
 
 select JSON_EXTRACT(question, "$.question.content[0]") as question,
        answer_options,
@@ -75,7 +75,7 @@ select JSON_EXTRACT(question, "$.question.content[0]") as question,
 from   items
 where  id = 82;
 
-/* To Do replace , with . and fraction e.g. 3/4 with decimal value and % with decimal value in correct_answer*/
+/* To Do replace , with . and fraction e.g. 3/4 with decimal value and % with decimal value in correct_answer */
 
 select id,
        correct_answer,
