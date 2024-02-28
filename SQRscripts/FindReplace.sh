944115e7a7c158ff21772529509c0a59d8d21a75
Sharon Klinkenberg <sharonklinkenberg@re-byodm-145-109-110-128.wireless.uva.nl>
2024-02-28 12:06:12 +0100

    added comment

--- SQRscripts/FindReplace.sh
+++ SQRscripts/FindReplace.sh
@@ -1,9 +1,9 @@
 
 
 
-SQRscripts/Probability/uva-multiplication-rule-716-nl/uva-multiplication-rule-716-nl.Rmd
+## Test file: SQRscripts/Probability/uva-multiplication-rule-716-nl/uva-multiplication-rule-716-nl.Rmd
 
-Only works directly in mac terminal not in Rstudio terminal
+## Only works directly in mac terminal not in Rstudio terminal
 
 find . -name '*.Rmd' -print0 | xargs -0 sed -i "" "s/\\\(/$/g"
 
